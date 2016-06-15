4dd3d452f8742d879e539a35f1b6fd5da80943bc
nondualit <anibal@nondualit.nl>
2016-06-15 11:30:44 +0000

    Add files via upload

---
+++ sb_cln_grw.sh
@@ -0,0 +1,394 @@
+#!/bin/ksh
+#!/usr/bin/ksh
+# bron: unix2004:/usr/local/bin»cat sb_cln_grw
+################################################################
+# Module    : sb_cln_grw
+# Doel      : Algemeen script tbv onderhoud groei-objecten AIX
+# Auteur    : G. Mosterd
+# Datum     : Thu Jan 25 08:22:01 NFT 2007
+################################################################
+# Omschrijving:
+################################################################
+# Wijzigingen
+################################################################
+# Datum       : 24-feb-2011
+# Auteur      : Ivo Breeden
+# Versie      : 1.1
+# Omschrijving: Add: option z=zip files. (using gzip)
+################################################################
+# Datum       : 11-mar-2011
+# Auteur      : Ivo Breeden
+# Versie      : 1.2
+# Omschrijving: Spaces in filenames are now handeled correctly.
+################################################################
+# Usage: sb_cln_grw -a{a|e|h|s|z} -d -n<num> -p<pattern> -s<source> [-t<target>] [-x]
+#
+#  -a {a|e|h|s|z} a=archive e=expire h=history s=shrink z=zip
+#    a = Move all files in -s<source-dir> older than -n<days> to -t<target-dir>
+#    e = Delete all files in -s<source-dir> older than -n<days>
+#    h = Rename all files in -s<source-dir> older than -n<days> to file.history
+#    s = Shrink the -s<source-file> to the last -n<lines>
+#    z = Zip all files in -s<source-dir> older than -n<days>
+#  -d Select directories also with -a{a|e|h}
+#  -n {NUMERIC-VALUE} number of days -a{a|e|h|z} or number of lines -a{s}
+#  -p {Select-pattern} not with -a{s}
+#  -s {source-object} directory with -a{a|e|h|z} or file with -a{s}
+#  -t {target-directory} only with -a{a}
+#  -x Execute mode. Without this option run is report only
+################################################################
+#  Verplaats alle bestanden ouder dan 30 dagen
+#  van /appl/xxx00t/log naar /appl/xxx00t/archief
+#  sb_cln_grw -aa -n30 -s/appl/xxx00t/log -t/appl/xxx00t/archief -x
+#
+#  Toon te Verwijderen bestanden ouder dan 60 dagen in /appl/yyy00p/log
+#  sb_cln_grw -ae -n60 -s/appl/yyy00p/log
+#
+#  Toon te Verwijderen bestanden en directories ouder dan 300 dagen
+#  met string "log" in de naam in directory /appl/yyy00p/log
+#  sb_cln_grw -ae -n60 -d -plog -s/appl/yyy00p/log
+#
+#  Verwijder alle bestanden ouder dan 60 dagen uit /appl/yyy00p/log
+#  sb_cln_grw -ae -n60 -s/appl/yyy00p/log -x
+#
+#  Hernoem alle bestanden ouder dan 45 dagen in /appl/yyy00p/log naar file.history
+#  sb_cln_grw -ah -n45 -s/appl/xxx00t/log -x
+#
+#  Reduceer /appl/xxx00t/log/big_logfile tot de laatste 10000 regels
+#  sb_cln_grw -as -n10000 -s/appl/xxx00t/log/big_logfile -x
+#
+#  Zip alle bestanden ouder dan 45 dagen in /appl/yyy00p/log 
+#  sb_cln_grw -az -n45 -s/appl/xxx00t/log -x
+#
+################################################################
+# Init default variables
+################################################################
+CURDIR=$(dirname $0)
+CURFIL=$(basename $0)
+[ "$CURDIR" = '.' ] && CURDIR=$(pwd)
+
+# Init global variables
+ACTION=""
+NUMBER=""
+SRCOBJ=""
+TARDIR=""
+PATTRN=""
+SUBDIR=""
+EXECUTE=""
+RUNTXT=""
+TMPFIL="/tmp/.$CURFIL.$$.tmp"
+TRGFIL="/tmp/.$CURFIL.$$.trg"
+TRGDAT=$(date)
+CLIPAR="$*"
+
+# trap "rm -f $TRGFIL TMPFIL" 0-255   # Opruimen bij exit
+trap "rm -f $TRGFIL TMPFIL" 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20   # Opruimen bij exit
+
+# #############################################
+# Function declarations
+# #############################################
+# #############################################
+# Usage display
+# #############################################
+Usage_Err()
+{
+  [ 0$1 -eq 0 ] && return
+  cat<<EOT
+Usage: $CURFIL -a{a|e|h|s|z} -d -n<num> -p<pattern> -s<source> [-t<target>] [-x]
+
+ -a {a|e|h|s|z} a=archive e=expire h=history s=shrink
+   a = Move all files in -s<source-dir> older than -n<days> to -t<target-dir>
+   e = Delete all files in -s<source-dir> older than -n<days>
+   h = Rename all files in -s<source-dir> older than -n<days> to file.history
+   s = Shrink the -s<source-file> to the last -n<lines>
+   z = Zip all files in -s<source-dir> older than -n<days>
+ -d Select directories also with -a{a|e|h}
+ -n {NUMERIC-VALUE} number of days -a{a|e|h|z} or number of lines -a{s}
+ -p {Select-pattern} not with -a{s}
+ -s {source-object} directory with -a{a|e|h|z} or file with -a{s}
+ -t {target-directory} only with -a{a}
+ -x Execute mode. Without this option run is report only
+
+EOT
+  ErrChk $*
+}
+
+# #############################################
+# Error check
+# #############################################
+ErrChk()
+{
+ if [ 0$1 -ne 0 ]
+ then
+   echo xxxxxxxxx
+   echo x ERROR x
+   echo xxxxxxxxx
+   LogMsg "RC=$*"
+   exit $1
+ fi
+
+}
+
+# #############################################
+# Log message
+# #############################################
+LogMsg()
+{
+  print "$(date +"%d/%m/%y %H:%M:%S"): $*"
+}
+
+# #############################################
+# Warning check
+# #############################################
+WrnChk()
+{
+ if [ 0$1 -ne 0 ]
+ then
+   banner WARNING
+   LogMsg "RC=$*"
+ fi
+ return $1
+}
+
+# #############################################
+# Calculate previous date
+# #############################################
+CreDatTrg()
+{
+  echo TRIGGER-DATE:$(date --date="$1 days ago")
+
+  # Create trigger
+  touch --date "$1 days ago"  $TRGFIL
+  ErrChk $? "ERROR: touch --date \"$1 days ago\" $TRGFIL FAILED!"
+}
+
+# ####################################################
+# Check CLI Parameters
+# ####################################################
+ChkPar()
+{
+  [[ $# -eq 0 ]] && Usage_Err 1 "ERROR: No parameters specified PRUTSER!"
+
+  #set -x
+
+
+  # Parse command line into arguments & check result of parsing
+  set -- `getopt a:dn:p:s:t:x $*`
+  Usage_Err $? "ERROR: Invalid parameter $CLIPAR specified DODO!"
+
+  typeset -l junc # lower case
+
+  while [ "$1" != -- ]
+  do
+    case $1 in
+    -a)     # Action
+            ACTION=$2
+            [[ $ACTION = a ]] && RUNTXT="ARCHIVE"
+            [[ $ACTION = h ]] && RUNTXT="HISTORY"
+            [[ $ACTION = e ]] && RUNTXT="EXPIRE"
+            [[ $ACTION = s ]] && RUNTXT="SHRINK"
+            [[ $ACTION = z ]] && RUNTXT="ZIP"
+            shift   # next flag
+            ;;
+    -d)     # Process subdirs
+            SUBDIR=1
+            ;;
+    -n)     # Numeric parameter
+            NUMBER=$2
+            shift   # next flag
+            ;;
+    -p)     # Pattern parameter
+            PATTRN=$2
+            shift   # next flag
+            ;;
+    -s)     # Source directory or file
+            SRCOBJ=$2
+            shift   # next flag
+            ;;
+    -t)     # Target directory
+            TARDIR=$2
+            shift   # next flag
+            ;;
+    -x)     # EXECUTE
+            EXECUTE=1
+            ;;
+    *)      # Parameter error
+            Usage_Err 1 "ERROR: Invalid parameter specified $1 DIPSTICK!"
+            ;;
+    esac
+    shift   # next flag
+  done
+
+  # Kontrole verplichte velden
+  [[ $ACTION != @(a|e|h|s|z) ]] && Usage_Err 1 "ERROR: Invalid action specified (-a $ACTION ) KNOEIER!"
+
+  # Kontrole bron object aanwezig
+  [[ ! -n $SRCOBJ ]] && Usage_Err 1 "ERROR: Source (-s <FILE|DIR> ) not specified PRUTSER!"
+
+  #[[ $SRCOBJ != /appl/* ]] && Usage_Err 1 "ERROR: Source object must be part of /appl/ KNUPPEL!"
+
+  E_TXT="Expiration days"
+
+  # Kontrole archiveren / expireren / history / reduceren / zippen
+  if [[ $ACTION = @(a|e|h) ]]
+  then
+    # archiveren / expireren / history / zippen
+    if [[ ! -d $SRCOBJ ]]
+    then
+      Usage_Err 1 "ERROR: Source (-s <DIR> ) is no directory KNUPPEL!"
+    fi
+    if [[ $ACTION = a ]]
+    then
+      if [[ ! -n $TARDIR ]] || [[ ! -d $TARDIR ]]
+      then
+        Usage_Err 1 "ERROR: Invalid directory (-t $TARDIR ) SHAKEHEAD!"
+      fi
+    fi
+  elif [[ $ACTION = s ]]
+  then
+    # reduceren
+    if [[ ! -f $SRCOBJ ]]
+    then
+      Usage_Err 1 "ERROR: Accessing file ( -s $SRCOBJ ) BOY!"
+    fi
+    E_TXT="Reduction "
+  elif [[ $ACTION = z ]]
+  then
+    # Zippen
+    if [[ -n $SUBDIR ]]
+    then
+      Usage_Err 1 "ERROR: Cannot zip directories NUT!"
+    fi
+    E_TXT="Zipping "
+  fi
+
+  # Kontrole number
+  if [[ $NUMBER != +([0-9]) ]]
+  then
+    Usage_Err 1 "ERROR: $E_TXT number invalid (-n $NUMBER ) BUFFEL!"
+  fi
+}
+
+# #############################################
+# Loop thru directory
+# #############################################
+Process_Directory()
+{
+  echo "SOURCE-DIR  :$SRCOBJ"
+  echo "TARGET-DIR  :$TARDIR"
+  echo "SUBDIRS-TO  :$SUBDIR"
+  echo "PATTERN     :$PATTRN"
+  echo "TRIGGER-DAY :$NUMBER"
+
+  # Create file with dir-content
+  ls -a $SRCOBJ >$TMPFIL
+  ErrChk $? "ERROR: ls >$TMPFIL FAILED!"
+
+  # Create date trigger
+  CreDatTrg $NUMBER
+
+  echo "##### SELECTED OBJECTS FOR $RUNTXT #####"
+
+  # Loop thru files in directory
+  while read d_obj
+  do
+    # Make fullpath
+    chk_obj=$SRCOBJ/$d_obj
+
+    # Skip current and parent dir
+    [[ $d_obj = . ]] || [[  $d_obj = .. ]] && continue
+
+    # When file mode Process files only
+    [[ ! -n $SUBDIR ]] && [[ ! -f $chk_obj ]] && continue
+
+    # Select specified pattern only
+    [ $PATTRN ] && [[ $d_obj != *$PATTRN* ]] && continue
+
+    # Select files older trigger only
+    [[ $chk_obj -nt $TRGFIL ]] && continue
+
+    # Select non history files only in history mode
+    [[ $ACTION = h ]] && [[ $d_obj = *.history ]] && continue
+
+    # Do not zip zipped files
+    [[ $ACTION = z ]] && [[ $d_obj = *.gz ]] && continue
+
+    ls -dl "$chk_obj"
+
+    # Select action only in execute mode
+    if [ $EXECUTE ]
+    then
+      if   [[ $ACTION = a ]]
+      then
+        mv "$chk_obj" $TARDIR
+        ErrChk $? "ERROR: mv $chk_obj $TARDIR FAILED!"
+      elif [[ $ACTION = e ]]
+      then
+        rm -rf "$chk_obj"
+        ErrChk $? "ERROR: rm -f $chk_obj FAILED!"
+      elif [[ $ACTION = h ]]
+      then
+        mv "$chk_obj" "${chk_obj}".history
+        ErrChk $? "ERROR: mv $chk_obj ${chk_obj}.history FAILED!"
+      elif [[ $ACTION = z ]]
+      then
+        gzip "$chk_obj"
+        ErrChk $? "ERROR: gzip $chk_obj FAILED!"
+      fi
+    fi
+  done<$TMPFIL
+  ErrChk $? "ERROR: Read $TMPFIL FAILED!"
+}
+
+# #############################################
+# Shrink file
+# #############################################
+Shrink_File()
+{
+  echo "SOURCE-FILE :$SRCOBJ"
+  echo "SHRINK-LINES:$NUMBER"
+
+  # Show file before
+  echo "BEFORE: $(ls -l $SRCOBJ)"
+  tail -$NUMBER $SRCOBJ > $TMPFIL
+  ErrChk $? "ERROR: tail -$NUMBER $SRCOBJ FAILED!"
+
+  if [ $EXECUTE ]
+  then
+    cp $TMPFIL $SRCOBJ
+    ErrChk $? "ERROR: cp $TMPFIL $SRCOBJ FAILED!"
+  fi
+
+  # Show file after
+  echo "AFTER : $(ls -l $SRCOBJ)"
+}
+
+########################################
+########################################
+########################################
+# MAIN
+########################################
+########################################
+########################################
+LogMsg "START: $CURFIL"
+
+echo "RUN-PAR     :$CURFIL $CLIPAR"
+
+# Perform CLI-checks
+ChkPar $*
+
+[ $EXECUTE ] && RUNMODE="EXECUTE" || RUNMODE="REPORT"
+
+echo "RUNMODE     :$RUNMODE"
+echo "ACTION      :$RUNTXT"
+
+# archiveren / expireren / history or reduce file
+if [[ $ACTION != s ]]
+then
+  Process_Directory
+else
+  Shrink_File
+fi
+
+rm -f $TMPFIL $TRGFIL 2>&1
+
+LogMsg "EINDE: $CURFIL"
---
+++ was_wash.sh
@@ -0,0 +1,351 @@
+#!/bin/ksh
+#*******************************************************************************
+# D e s c r i p t i o n
+#*******************************************************************************
+# Scriptname     : was_wash.sh
+# Current version: 1.07
+# Function       : Clean old logfiles.
+#                  First old logfiles are gzipped,
+#                  then very old files are deleted.
+#                  Last (when applicable) empty directories are removed.
+# Location       : Location of stopwebsphere.sh
+# Parameters     : -
+# Calls scripts  : sb_cln_grw.sh
+# Called by      : stopwebsphere.sh
+#
+#*******************************************************************************
+# C h a n g e   l o g
+#*******************************************************************************
+# Version  Date        Name              Description
+# -------  ----------  ----------------  ---------------------------------------
+#    1.00  10-01-2011  Ivo Breeden       Eerste versie
+#    1.01  25-02-2011  Ivo Breeden       Maak gebruik van standaard script
+#                                        sb_cln_grw.sh
+#    1.02  03-03-2011  Ivo Breeden       Toev: zip ${wasbase}/???/log/jacl.
+#                                        Ruim ${wasbase}/???/log/jacl op als
+#                                        er meer dan 5MB staat.
+#                                        In http logdir staat ook pid file,
+#                                        die mag niet gezipped worden.
+#                                        Zip age 3 --> 15 dagen.
+#    1.03  19-05-2011  Ivo Breeden       Change: Zip and clean the stop/start
+#                                        Er werd teveel logging geproduceerd
+#                                        omdat er teveel directories waren
+#                                        waar niets gedaan hoefde te worden.
+#                                        Daarom wordt nu met `find` een selectie
+#                                        gemaakt van directories waar zip
+#                                        of erase moet worden uitgevoerd.
+#    1.04  22-06-2011  Ivo B/Ali Kokhan  Toev: Ditzo directories.
+#    1.05  28-06-2011  Ivo Breeden       Change: efficiency verbetering:
+#                                        eerst verwijderen, dan pas zippen.
+#                                        (I.p.v. omgekeerd.)
+#    1.06  06-07-2011  Ivo Breeden       Change: geparameteriseerd.
+#                                        Directories staan nu in *.ini file.
+#    1.07  14-07-2011  Ivo Breeden       Diverse verbeteringen.
+#    1.08  02-09-2015  Ivo Breeden       Aangepast voor algemeen gebruik, dus
+#                                        niet meer voor WAS. $wasid veranderd
+#                                        in LOGNAME.
+#*******************************************************************************
+# was_wash.sh is a front-end for the general cleanup script sb_cln_grw
+################################################################
+# Usage: sb_cln_grw -a{a|e|h|s|z} -d -n<num> -p<pattern> -s<source> [-t<target>] [-x]
+#
+#  -a {a|e|h|s} a=archive e=expire h=history s=shrink
+#    a = Move all files in -s<source-dir> older than -n<days> to -t<target-dir>
+#    e = Delete all files in -s<source-dir> older than -n<days>
+#    h = Rename all files in -s<source-dir> older than -n<days> to file.history
+#    s = Shrink the -s<source-file> to the last -n<lines>
+#    z = Zip all files in -s<source-dir> older than -n<days>
+#  -d Select directories also with -a{a|e|h}
+#  -n {NUMERIC-VALUE} number of days -a{a|e|h|z} or number of lines -a{s}
+#  -p {Select-pattern} not with -a{s}
+#  -s {source-object} directory with -a{a|e|h|z} or file with -a{s}
+#  -t {target-directory} only with -a{a}
+#  -x Execute mode. Without this option run is report only
+################################################################
+
+
+# Invoke initialization, common to stop & start scripts
+#
+# First retrieve real location of script
+if [[ -L ${0} ]]; then
+  ls -l ${0}|read x x x x x x x x x x linkdir
+  linkdir=$(dirname ${linkdir})
+else
+  linkdir=$(cd $(dirname ${0}) && pwd -P)
+fi
+
+scriptbase=$(basename ${0})
+# scriptpure is without .sh suffix (used to find the
+#   accompanied .ini file)
+scriptpure=${scriptbase%.sh}
+
+# Parameters: 1 possible parameter: -sim
+if [[ $1 == "-sim" ]] ; then
+  export sim='echo '
+  echo "Simulatie (er wordt niet echt iets verwijderd):"
+fi
+
+################################################################
+#
+# Functions
+#
+# non_recursive_du() shows disk usage (du) of a directory
+#                    without de subdirectories in bytes
+#                    Only regular files are counted,
+#                    not symbolic links, directories, 
+#                    special files or hidden files (whose
+#                    name starts with '.').
+# explanation:
+#  ls -l shows a list of files and directories.
+#  if the first column starts with "-" it is a file,
+#  and the size in bytes is added to produce the result.
+#
+function non_recursive_du {
+  ls -l $1 |
+  awk 'BEGIN                   {resultaat=0} 
+       substr($1, 1, 1) == "-" {bytes=$5; resultaat+=bytes} 
+       END                     {printf("%d\n", resultaat)}'
+}
+
+#
+################################################################
+# Simple cleanup; cleanup files in one directory
+# Files, older than ${DELAGE} days are deleted. 
+# Files, older than ${ZIPAGE} days are zipped.
+#
+function wash_simple {
+  DIRECTORY=${1}
+  ZIPAGE=${2}
+  DELAGE=${3}
+  if [ -z "${ZIPAGE}" ]
+  then 
+    ZIPAGE=-1
+  fi
+  if [ -z "${DELAGE}" ]
+  then
+    DELAGE=-1
+  fi
+
+  # Expand wildcards and substitute variables
+  set $(eval echo ${DIRECTORY})
+  for dir in "${@}"
+  do
+    if [ -d "${dir}" ]
+    then
+      if (( ${DELAGE} >= 0 ))
+      then
+        # is ther work to do? (this is to prevent abundant logging)
+        todo=$(find "${dir}" ! -name "$(basename "${dir}")" -prune -type f -mtime +${DELAGE}  -print| head -1)
+        if [ ! -z "${todo}" ]
+        then
+          if [[ -z ${sim} ]]
+          then
+            #Als $sim leeg is, dan met -x (execute) optie. Anders niet.
+            ${linkdir}/sb_cln_grw.sh -a e -n ${DELAGE} -s "${dir}" -x
+          else
+            ${sim}${linkdir}/sb_cln_grw.sh -a e -n ${DELAGE} -s "${dir}"
+            ${linkdir}/sb_cln_grw.sh -a e -n ${DELAGE} -s "${dir}"
+          fi
+          echo '================================================================================'
+        fi
+      fi
+      if (( ${ZIPAGE} >= 0 ))
+      then
+        # is ther work to do? (this is to prevent abundant logging)
+        todo=$(find "${dir}" ! -name "$(basename "${dir}")" -prune -type f -mtime +${ZIPAGE} ! -name '*.gz' -print| head -1)
+        if [ ! -z "${todo}" ]
+        then
+          if [[ -z ${sim} ]]
+          then
+            #Als $sim leeg is, dan met -x (execute) optie. Anders niet.
+            ${linkdir}/sb_cln_grw.sh -a z -n ${ZIPAGE} -s "${dir}" -x
+          else
+            ${sim}${linkdir}/sb_cln_grw.sh -a z -n ${ZIPAGE} -s "${dir}"
+            ${linkdir}/sb_cln_grw.sh -a z -n ${ZIPAGE} -s "${dir}"
+          fi
+          echo '================================================================================'
+        fi
+      fi
+    fi
+  done
+}
+
+#
+################################################################
+# Recursive cleanup; cleanup files in subdirectories. 
+# (And te directory itself.) Delete empty directories.
+#
+function wash_recursive {
+  DIRECTORY=${1}
+  ZIPAGE=${2}
+  DELAGE=${3}
+  if [ -z "${ZIPAGE}" ]
+  then 
+    ZIPAGE=-1
+  fi
+  if [ -z "${DELAGE}" ]
+  then
+    DELAGE=-1
+  fi
+
+  # Expand wildcards and substitute variables
+  set $(eval echo ${DIRECTORY})
+  for dir in "${@}"
+  do
+    # Process subdirectories of the expanded $DIRECTORY(s)
+    find "${dir}" -path "*lost+found" -prune -o  -type d | while read sdir
+    do
+      # do not try to process lost+found directories
+      if [[ "${sdir}" = *lost+found ]]
+      then
+        continue
+      else
+        wash_simple "${sdir}" ${ZIPAGE} ${DELAGE}
+        # Remove empty directories
+        if (( $(ls -a "${sdir}" |wc -l) == 2 ))  # only . and ..
+        then
+          ${sim}rmdir "${sdir}"
+          echo "Removed empty directory ${sdir}"
+          echo '================================================================================'
+        fi
+      fi
+    done
+  done
+}
+
+################################################################
+# Cleanup to maximum size: first old files are zipped.
+# Then as long as the maximum size is exceeded, the oldest
+# file is deleted.
+# Only regular files are counted, not symbolic links, directories, 
+# special files or hidden files (whose name starts with '.').
+#
+function wash_to_size {
+  DIRECTORY=${1}
+  ZIPAGE=${2}
+  MAXSIZE=${3}
+  if [ -z "${ZIPAGE}" ]
+  then 
+    ZIPAGE=-1
+  fi
+  if [ -z "${MAXSIZE}" ]
+  then
+    MAXSIZE=-1
+  fi
+
+  # Expand wildcards and substitute variables
+  set $(eval echo ${DIRECTORY})
+  for dir in "${@}"
+  do
+    if [ -d "${dir}" ]
+    then
+      # Start the zip program on the directory
+      wash_simple "${dir}" ${ZIPAGE} -1
+      #
+      # Determine the size of all files in the directory
+      totalsize=$(non_recursive_du "${dir}")
+      if (( ${MAXSIZE} > 0 ))
+      then
+        if (( ${totalsize} > ${MAXSIZE} ))
+        then
+          cd "${dir}"
+          print "Directory ${dir} bevat ${totalsize} bytes. Directory wordt geschoond tot onder ${MAXSIZE} bytes."
+          let teveelB=${totalsize}-${MAXSIZE}
+          # the string "victims" will contain files to be deleted
+          victims=""
+          # list files, oldest first
+          ls -ltr | while read RWX x x x huidigefileB x x x bestand
+          do
+            if [[ ${RWX} == -* ]]  # if it is a regular file
+            then
+              # mind the quotes; a filename may contain spaces
+              victims=${victims}\"${bestand}\"" "
+              let teveelB=${teveelB}-${huidigefileB}
+              # if enough bytes have been gathered for cleanup, then quit
+              if (( ${teveelB} <= 0 ))
+              then
+                break
+              fi
+            fi
+          done
+          echo "De volgende files worden verwijderd in ${dir}:"
+          # the string $victims may be large, so don't rely
+          # on `ls -l $victims`. The same goes for remove.
+          cat <<EOF |xargs ls -l
+          ${victims}
+EOF
+          cat <<EOF |xargs ${sim}rm -f
+          ${victims}
+EOF
+          echo '================================================================================'
+          # back to the directory where the `if` started
+          cd -   >/dev/null
+        fi
+      fi
+    fi
+  done
+}
+
+#
+################################################################
+# Touch: touch files to give them a new filestamp.
+# This is to prevent open files to be zipped or deleted.
+# Caveat: it does not have the desired result when directories
+# are cleaned with zipage=0 or delage=0.
+# Overall this is not a very good solution. But it is a
+# practical solution.
+#
+function wash_touch {
+  BESTAND=${1}
+
+  # Expand wildcards and substitute variables
+  set $(eval echo ${BESTAND})
+  for ffile in "${@}"
+  do
+    if [ -f "${ffile}" ]
+    then
+      ${sim}touch "${ffile}"
+      echo "File ${ffile} touched."
+      echo '================================================================================'
+    fi
+  done
+}
+
+#
+# End Functions
+################################################################
+# Start Script
+#
+
+# Read the ini file
+#
+awk -F: 'substr($1, 1, 1) != "#" {printf "%s %s %s %s %s\n", $1, $2, $3, $4, $5}' ${linkdir}/${scriptpure}.ini |
+  while read Pwasid Paction Pzip Pdelete Pdirectory 
+  do
+    # process the line if this environment (LOGNAME) is addressed
+    if (( $(expr length ${LOGNAME}) == $(expr match ${LOGNAME} "${Pwasid}") ))
+    then
+      case ${Paction} in
+      S|s)
+        wash_simple "${Pdirectory}" ${Pzip} ${Pdelete}
+        ;;
+      R|r)
+        wash_recursive "${Pdirectory}" ${Pzip} ${Pdelete}
+        ;;
+      M|m)
+        wash_to_size "${Pdirectory}" ${Pzip} $((${Pdelete} * 1024))
+        ;;
+      T|t)
+        wash_touch "${Pdirectory}"
+        ;;
+      *)
+        print ERROR: invalid action in ${scriptpure}.ini: ${Paction}
+        ;;
+      esac
+    fi
+  done
+
+#
+# End Script
+################################################################
+
